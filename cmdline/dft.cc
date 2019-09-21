6288298efb491202ae77a65594f018de3c8bb132
Ivo <cetko13@gmail.com>
2019-09-21 16:22:59 -0300

    dft idft  correcciones varias (todavia no chequeado)

--- cmdline/dft.cc
+++ cmdline/dft.cc
@@ -3,22 +3,22 @@
 &Array _dft(const Array &x, int flag){
 
 	int N=getSize(x);
-	Array_complejo transf=new Array_complejo(N);
-	Array_complejo w_nk=new Array_complejo(N);
+	Array<complejo> transf=Array(N);//constructor por tamano
+	Array<complejo> w_nk=Array(N);
 
-	for(k=1, arg=2*PI*k/N;	k==N	; k++){
-		for(n=1;k==N;n++){
+	for(k=0, arg=2*PI*k/N;	k==N-1	; k++){
+		for(n=0;n==N-1;n++){
 			w_nk[k]+=x(n)*complejo(cos(arg*n),flag*cos(arg*n));
 		}
 	}
 
-	return &transformada;
+	return &transf;
 }
 
 &Array dft(const Array &x){
 	return _dft(const Array &x, 1);
 }
 
-&Array dft(const Array &x){
-	return _idft(const Array &x, -1)/getSize(x); //conjuga W_nk
+&Array idft(const Array &x){
+	return _dft(const Array &x, -1)/getSize(x); //conjuga W_nk
 }
