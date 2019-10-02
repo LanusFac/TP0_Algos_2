8a6ae6d69a4dcd31926c7bbc471af9d598809beb
Ivo <cetko13@gmail.com>
2019-10-02 13:52:33 -0300

    pseudomain para testear op>> op<<  dft, e idft

--- rochi/main_ivo.cpp
+++ rochi/main_ivo.cpp
@@ -1,12 +1,12 @@
 #include "complejo.h"
 #include "array.h"
 #include "dft_facu.h"
-#include "leer_cmdline.h"
+//#include "leer_cmdline.h"
 
 #include "complejo.cc"
 #include "array.cpp"
 #include "dft.cc"
-#include "leer_cmdline.cpp"
+//#include "leer_cmdline.cpp"
 
 #include <iostream>
 #include <iomanip>
@@ -25,32 +25,24 @@ using namespace std;
 int main(int argc, char *argv[]) {
     Array <complejo> arr_com; //la que voy a transformar
     complejo com;
-    ifstream iFile;
-    ofstream oFile;
+    ifstream iFile("in.txt");
+    ofstream oFile("out.txt");
 
-    int metodo_elegido = leer_cmdline (argc, argv, &iFile, &oFile);
-    
+   // int metodo_elegido = leer_cmdline (argc, argv, &iFile, &oFile);
+
     iFile>>arr_com;
     
     Array<complejo> arrayComplejosTransformados;
 
-    if (metodo_elegido == 1)
+    Array<complejo> arrayComplejosAntiTransformados;
+
         arrayComplejosTransformados = dft(arr_com);
 
-    else if (metodo_elegido == -1)
-        arrayComplejosTransformados = idft(arr_com);
+        arrayComplejosAntiTransformados = idft(arr_com);
 
-
-    for (int h = 0; h < arr_com.getSize(); h++)
-        oFile << arr_com[h] << endl;
-
-    oFile << SEPARADOR << endl;
-
-
-    for (int h = 0; h < arrayComplejosTransformados.getSize(); h++)
-        oFile << arrayComplejosTransformados[h] << endl;
-
-    return 0;
+    oFile<<"input:  "<<arr_com<<endl;
+    oFile<<"output: "<<arrayComplejosTransformados<<endl;
+    oFile<<"antitf: "<<arrayComplejosAntiTransformados<<endl;
 
 }
 
