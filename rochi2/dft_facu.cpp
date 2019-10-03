40f9f29ce25bf993c84fc71606cb470a6d59200e
Unknown <facundolanus@gmail.com>
2019-10-03 14:17:39 -0300

    detalles

--- rochi2/dft_facu.cpp
+++ rochi2/dft_facu.cpp
@@ -11,8 +11,8 @@ Array<complejo> & _dft(Array<complejo> &x)
 
 
 	for(int k=0; k <= N-1; k++){
-		double arg=2*PI*k/N;
-
+		double arg = -2 * PI * k / N;
+ 
 		for(int n=0; n <= N-1; n++){
 			w_nk[k] += x[n]*complejo(cos(arg*n),sin(arg*n));
 		}
