5e56be5fea392187cd83685f97f5415c9894e299
maiten <RosarioSzuplat>
2019-10-03 14:09:15 -0300

    mmm

---
+++ rochi2/cargar_array.cpp
@@ -0,0 +1,22 @@
+//funcion para leer de a un solo vector desde archivo de entrada
+//recibe archivo del que debe leer y devuelve array de complejos leido
+//devuelve NULL cuando se llega al final del archivo
+//si encuentra un error en un vector imprime msj y pasa al siguiente
+
+ /* while(*iFile >> com){ //inicializo array de complejos
+
+    Array <complejo> arr_com_aux(arr_com.getSize() + CHOP_SIZE);
+
+    for (int i = 0; i < arr_com.getSize(); i++)
+        arr_com_aux[i] = arr_com[i];
+
+    arr_com_aux[arr_com.getSize()] = com;
+
+    arr_com = arr_com_aux;
+    // delete[] arr_com_aux; con & compila pero tira seg.f
+
+}*/
+
+Array <complejo> cargar_array(istream iFile){
+    
+}
